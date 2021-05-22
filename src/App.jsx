c258de78618cf59df3b01466d2d01bac5cc796ba
nisha saraswat <nisha0217@gmail.com>
2021-05-22 23:53:07 +0200

    Photon project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useEffect } from 'react';
+import mongoosy from 'mongoosy/frontend';
+const { Login } = mongoosy;
+
+ function App() {
+
+  return (
+    <div className="App">
+      <h1>hello</h1>
+    </div>
+  );
+}
+export default App
