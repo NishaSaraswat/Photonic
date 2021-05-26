1525bdca4aac3c74104486157f37ffb092d20cc2
HILLELWIE_DAHLIN <HILLEWIE.DAHLIN@LIVE.SE>
2021-05-26 23:09:00 +0200

    feed changes

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
 import Post from './Post'
-import Group2 from '../styleapp/icons/Group2.png'
 import '../styleapp/HomePage.css'
-import Photos from '../components/Photos'
+import LogoHeader from '../components/LogoHeader'
 
 const HomePage = () => {
     return (
         
-        
-        <div className="Main">
-           <img src={Group2} alt="group2" id="group2"></img>
+    <div className="Main">
+        <LogoHeader/>
            
-            
-
-            <Post/>
+         <Post/>
 
         </div>
         
