e03294b36024bbe2bcb6c9c7d2fef5ddcc1f4348
Viviann Rydh <rydh.vivian@gmail.com>
2021-05-28 22:26:49 +0200

    update photos component

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
+import mongoosy from 'mongoosy/frontend';
+const { User, Photo } = mongoosy;
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
 import DeleteIcon from '@material-ui/icons/Delete';
 
 
-const Photos = ({photos,time,userName}) => {
+
+const Photos = () => {
+    const [allPhotos, setAllPhotos]=useState([]);
+    const getAllPhotos=async ()=>{
+        let photos=await Photo.find();
+        console.log(photos);
+        setAllPhotos(photos)
+    }
+    useEffect(() => {
+        getAllPhotos();
+    }, [])
     return (
         <div>
-             {photos.map(photo =><div key={photo.url}>
+             {allPhotos.map(photo =><div key={photo.url}>
                 <h5>created by: {photo.auther}</h5>
                 <img src={'/uploads/' + photo.url} style={{width:'90%'}}/>
                 <h5>{photo.url}</h5>
-                <span>{time}</span>
                 <FavoriteBorderIcon />
                 <ThumbUpIcon />
                 <DeleteIcon />
